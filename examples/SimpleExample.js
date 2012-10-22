7a8ea0a41b5855168e05cb9ecc6d5f0f93ff681b
Flexmotion <felix.nielsen@rwatgg.dk>
2012-10-22 12:02:36 +0200

    Example update.

---
+++ examples/SimpleExample.js
@@ -0,0 +1,40 @@
+$(window).ready(function()
+{
+	$.getJSON("assets/json/data.json", bind(window, window.onLoaded));
+});
+
+function onLoaded(data)
+{
+	JW.init(	{
+					enabledDebug: true,
+					title: "Title when node has no Title",
+					//enabled google analytics.
+					enableTracking: true
+				}, data, $("body"));
+}
+
+var AnimationPage = JW.default.BasicPage.extend(
+{
+	animate: function()
+	{
+		//don't call this._super if you wanna animate.
+		$(this._el).stop(true);
+
+		if(this._state === "ANIMATING_IN")
+		{
+			$(this._el).css("opacity", 0).animate({"opacity": 1},
+			{
+				duration: 800,
+				complete: bind(this, this.onAnimatedIn)
+			});
+		}
+		else if(this._state === "ANIMATING_OUT")
+		{
+			$(this._el).css("opacity", 1).animate({"opacity": 0},
+			{
+				duration: 800,
+				complete: bind(this, this.onAnimatedOut)
+			});
+		}
+	}
+});
